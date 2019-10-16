ff88946b30fd075ca98fe96e8faa8740ab5f58fc
Ubuntu <ubuntu@ip-172-31-18-91.ec2.internal>
2019-10-16 18:16:45 +0000

    Initial code

---
+++ constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Matsuru Hoshi
+# Created on: Oct 2019
+# This constants file is for Space Alien game
+
+# CircuitPython screen size is 160x120 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 120
+SCREEN_GRID_X = 16
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
